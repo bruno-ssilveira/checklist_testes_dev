de7c20f975795cc41ad8c11ba12b8fd95b8f2fba
Bruno Silveira <bruno.santossilveira001@gmail.com>
2025-02-20 15:55:38 -0300

    atualizacao do bruno

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Corrigido para usar Routes ao invés de Switch
-import Home from './pages/Home';
-import Checklist from './pages/Checklist';
-import Checklist2 from './pages/Checklist2';
-import Checklist3 from './pages/Checklist3';
-import Checklist4 from './pages/Checklist4';
-import Conclusao from './pages/Conclusao';
-import GlobalStyles from './styles/globalStyles';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom"; // Agora importando corretamente
+import Home from "./pages/Home";
+import Checklist from "./pages/Checklist";
+import Checklist2 from "./pages/Checklist2";
+import Checklist3 from "./pages/Checklist3";
+import Checklist4 from "./pages/Checklist4";
+import Conclusao from "./pages/Conclusao";
+import GlobalStyles from "./styles/globalStyles";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter> {/* Envolva com BrowserRouter */}
-      <Routes> {/* Use Routes para definir as rotas */}
+    <HashRouter> {/* Substituído BrowserRouter por HashRouter */}
+      <Routes> {/* Mantendo as rotas dentro de Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/checklist" element={<Checklist />} />
         <Route path="/checklist2" element={<Checklist2 />} />
@@ -21,9 +21,6 @@ createRoot(document.getElementById('root')).render(
         <Route path="/conclusao" element={<Conclusao />} />
       </Routes>
       <GlobalStyles />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
-
-
-
